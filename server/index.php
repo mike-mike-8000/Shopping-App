2003b94789f3fb74a302a4d1a94821f835a6b488
Michael Oluoch <78139141+mike-mike-8000@users.noreply.github.com>
2022-07-01 16:25:05 +0300

    Add files via upload

---
+++ server/index.php
@@ -0,0 +1,20 @@
+<?php
+  $name = 'John';
+  echo '<h1>Hello World</h1>';
+  echo "<h2>My name is $name</h2>";
+
+  // index based
+  $routes = [];
+  echo $routes[0];
+  //associative array
+  $routes = ['hello']
+
+
+  class Application {
+      public function run() {
+          echo '<h1>Hello World!</h1>'
+      }
+  }
+
+  $app = new Application()
+?>
